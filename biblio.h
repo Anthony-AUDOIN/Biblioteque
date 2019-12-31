c73b6e66b450e006a5a86043cfbdcb9203db8dc1
samdutinfolimoges <58813932+samdutinfolimoges@users.noreply.github.com>
2019-12-31 15:47:32 +0100

    Add files via upload

--- biblio.h
+++ biblio.h
@@ -7,6 +7,7 @@ using namespace std;
 
 const int maxlivres = 100;
 
+
 struct t_date {
 	int jj;
 	int mm;
@@ -17,18 +18,21 @@ struct t_auteur {
 	int id;
 	string nom;
 	string prenom;
+	string nationalite;
 	t_date datedenaissance;
+	t_date datedemort;
 };
 
 struct t_livre {
 	string titre;
-	t_auteur auteur;
+	t_auteur id;
 	string style;
 	int pages;
 	string editeurlivre;
 	t_date datedeparution;
 	string etat;
-	int note;
+	int note[maxlivres];
+	int moynotes;
 };
 
 
@@ -40,15 +44,20 @@ struct t_biblio {
 };
 
 void menu(int minchoix, int maxchoix, string menu, int& choix);
-void affichelivre(t_livre monlivre, t_auteur monauteur);
+int calculmoyennenotes(int nbnote, t_livre monlivre);
+void affichelivre(t_livre monlivre, int nbnote);
 void affichelivres(t_biblio mabib);
 //void affichelivresauteur(t_biblio mabib, t_livre monlivre);
-void ajoutelivre(t_biblio& mabib);
-int recherchelivre(t_biblio mabib);
+void ajoutelivre(t_biblio& mabib, string titre, int nbnote);
+int recherchelivre(t_biblio mabib, string titre);
 void supprlivre(t_biblio& mabib);
 void recherche(t_biblio mabib);
 void emprunterlivre(t_biblio& mabib);
 void rendrelivre(t_biblio& mabib);
-//void vousaimerezaussi(t_biblio, int i);
+void afficheauteur(t_auteur monauteur);
+void afficheauteurs(t_biblio mabib);
+void notelivre(t_biblio& mabib, int nbnote);
+//void suggestion(t_biblio, int i);
 //void sauvegarde(t_biblio& mabib);
-#endif 
+
+#endif
--- main.cpp
+++ main.cpp
@@ -4,41 +4,50 @@
 
 using namespace std;
 
-int main() { 
-	
+int main() {
+
 	system("color F0");
 
-
+	int nbnote = 0;
+	string titre;
 	t_auteur monauteur;
 	t_biblio mabib;
 	mabib.nbelem = 0;
 	mabib.nbauteur = 0;
 	int choix;
 	do {
-		menu(1, 7, "|------------------------------------------------------------------|\n			BIBLIOTHEQUE			\n#################################################################\n			  ACCUEIL					\n1 : Afficher l'ensemble des livres de la bibilotheque\n2 : Ajouter un livre\n3 : Supprimer un livre\n4 : Recherche\n5 : Emprunter un livre\n6 : Rendre un livre\n7 : Quitter", choix);
+		menu(1, 9, "|------------------------------------------------------------------|\n			BIBLIOTHEQUE			\n#################################################################\n			  ACCUEIL					\n1 : Afficher l'ensemble des livres de la bibilotheque\n2 : Ajouter un livre\n3 : Afficher l'ensemble des auteurs de la biblioteque\n4 : Supprimer un livre\n5 : Recherche\n6 : Emprunter un livre\n7 : Rendre un livre\n8 : Noter un livre\n9 : Quitter", choix);
 		switch (choix) {
 		case 1: affichelivres(mabib);
 			system("pause");
 			break;
-		case 2: ajoutelivre(mabib);
+		case 2: ajoutelivre(mabib, titre, nbnote);
 			system("pause");
 			break;
-		case 3:supprlivre(mabib);
+		case 3: afficheauteurs(mabib);
 			system("pause");
 			break;
-		case 4: recherche(mabib);
+		case 4:supprlivre(mabib);
 			system("pause");
 			break;
-		case 5: emprunterlivre(mabib);
+		case 5: recherche(mabib);
+			system("pause");
 			break;
-		case 6: rendrelivre(mabib);
+		case 6: emprunterlivre(mabib);
+			system("pause");
 			break;
-		case 7:
+		case 7: rendrelivre(mabib);
+			system("pause");
+			break;
+		case 8: notelivre(mabib, nbnote);
+			nbnote++;
+			system("pause");
+			break;
+		case 9:
 			break;
 		}
 		system("cls");
-	}
-	while (choix != 7);
+	} while (choix != 9);
 	return 0;
-	
+
 }
